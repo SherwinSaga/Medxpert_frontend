8206a26194e40e8c86a42a2859c91cc92bc44c82
CastroNicole <you@example.com>
2024-05-09 21:19:38 +0800

    Login

--- src/Components/HeaderFooter/navigation.jsx
+++ src/Components/HeaderFooter/navigation.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import Container from 'react-bootstrap/Container';
 import {  useNavigate } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
--- src/Components/LoginRegister/login.jsx
+++ src/Components/LoginRegister/login.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import { Link, useNavigate} from 'react-router-dom';
-
+import { FaUserTag } from "react-icons/fa";
+import { FaUserShield } from "react-icons/fa";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import video from '../../assets/vid.mp4';
+import logo from '../../assets/logo1.png';
+import "./login.css"
 
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [loginStatus, setLoginStatus] = useState('');
 
     const navigate = useNavigate();
 
@@ -34,32 +40,64 @@ function Login() {
     };
     
 
-
     return (
-        <div className="login-container">
-            <h6 className='loginHeader'>LOGIN</h6>
-            <form onSubmit={handleSubmit}>
-                <label className="userName">
-                    USERNAME:
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                        placeholder="Enter Username"
-                    />
-                </label>
-                <label className="passWord">
-                    PASSWORD:
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        placeholder="Enter password"
-                    />
-                </label>
-                <input type="submit" value="SUBMIT" id="btnSubmit"/>
-            </form>
-            <p id="noAcc">Don't have an account? <Link to="/register">Register</Link></p>
+        <div className="loginPage flex" style={{ display: 'flex', alignItems: 'center' }}>
+            <div className="container flex" style={{ display: 'flex', alignItems: 'center' }}>
+
+                <div className="videoDiv">
+                    <video src={video} autoPlay muted loop></video>
+                    <div className="textDiv">
+                        <h2 className="title">Pharmaceutical Information Platform</h2>
+                        <p>Unleashing knowledge, One medication at a time</p>
+                    </div>
+
+                    <div className="footerDiv flex" style={{ display: 'flex', alignItems: 'center' }}>
+                        <span className="text">Don't have an account?</span>
+                        <Link>
+                            <button className="btn">Sign Up</button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="formDiv flex" style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className="headerDiv" >
+                        <h3>Welcome Back!</h3>
+                    </div>
+                    {loginStatus && <div className="error">{loginStatus}</div>}
+                    <form action="" className="form grid">
+
+                        <div className="inputDiv">
+                            <label htmlFor="username">Username:</label>
+                            <div className="input flex" style={{ display: 'flex', alignItems: 'center' }}>
+                                <FaUserTag className="icon"/>
+                                <input type="text" id="username" placeholder="Enter Username" onChange={(e) => {
+                                    setUsername(e.target.value)
+                                }}/>
+                            </div>
+                        </div>
+
+                        <div className="inputDiv">
+                            <label htmlFor="password">Password:</label>
+                            <div className="input flex" style={{ display: 'flex', alignItems: 'center' }}>
+                                <FaUserShield className="icon"/>
+                                <input type="password" id="password" placeholder="Enter Password" onChange={(e) =>{
+                                    setPassword(e.target.value)
+                                }}/>
+                            </div>
+                        </div>
+
+                        <button type="submit" className="btn flex" style={{ display: 'flex', alignItems: 'center' }} onClick={(e) => handleSubmit(e)}>
+                            <span>Login</span>
+                            <MdKeyboardDoubleArrowRight className="icon"/>
+                        </button>
+
+                        <span className="forgotPassword">
+                            Forgot your password? <a href="">Click Here</a>
+                        </span>
+                    </form>
+                </div>
+
+            </div>
         </div>
     );
 }
