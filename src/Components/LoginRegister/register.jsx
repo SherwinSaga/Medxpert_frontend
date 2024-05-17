b003feef46dc017c5d82f2d14561b065a6034e6a
CastroNicole <you@example.com>
2024-05-17 18:58:38 +0800

    Register

--- src/Components/LoginRegister/register.jsx
+++ src/Components/LoginRegister/register.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
 
 function Register() {
-  return (
-    <div>R</div>
-  )
+    return (
+        <div className="loginPage flex" style={{ display: 'flex', alignItems: 'center' }}>
+            <div className="container flex" style={{ display: 'flex', alignItems: 'center' }}>
+
+                <div className="videoDiv">
+                    <video src={video} autoPlay muted loop></video>
+                    <div className="textDiv">
+                        <h2 className="title">Pharmaceutical Information Platform</h2>
+                        <p>Unleashing knowledge, One medication at a time</p>
+                    </div>
+
+                    <div className="footerDiv flex" style={{ display: 'flex', alignItems: 'center' }}>
+                        <span className="text">Have an account?</span>
+                        <Link>
+                            <button className="btn">Login</button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="formDiv flex" style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className="headerDiv" >
+                        <h3>Let Us Know You!</h3>
+                    </div>
+                    {loginStatus && <div className="error">{loginStatus}</div>}
+                    <form action="" className="form grid">
+
+                        <div className="inputDiv">
+                            <label htmlFor="username">Username:</label>
+                            <div className="input flex" style={{ display: 'flex', alignItems: 'center' }}>
+                                <FaUserTag className="icon"/>
+                                <input type="text" id="username" placeholder="Enter Username" onChange={(e) => {
+                                    setUsername(e.target.value)
+                                }}/>
+                            </div>
+                        </div>
+
+                        <div className="inputDiv">
+                            <label htmlFor="password">Password:</label>
+                            <div className="input flex" style={{ display: 'flex', alignItems: 'center' }}>
+                                <FaUserShield className="icon"/>
+                                <input type="password" id="password" placeholder="Enter Password" onChange={(e) =>{
+                                    setPassword(e.target.value)
+                                }}/>
+                            </div>
+                        </div>
+
+                        <button type="submit" className="btn flex" style={{ display: 'flex', alignItems: 'center' }} onClick={(e) => handleSubmit(e)}>
+                            <span>Register</span>
+                            <MdKeyboardDoubleArrowRight className="icon"/>
+                        </button>
+
+                        <span className="forgotPassword">
+                            Forgot your password? <a href="">Click Here</a>
+                        </span>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default Register;
